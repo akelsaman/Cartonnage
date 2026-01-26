c8b84930b67b3df1275fe895b3328bd505f168bf
Ahmed Kamal ELSaman <ak.elsaman@gmail.com>
2026-01-27 00:43:16 +0200

    202601270043

--- cartonnage/cartonnage_test.py
+++ cartonnage/cartonnage_test.py
@@ -55,15 +55,15 @@ class Managers(Employees): pass
 employeeManagerRelation = (Employees.manager_id == Managers.employee_id )
 
 ### SQLite
-raw_sql = """
-INSERT INTO Employees (employee_id, first_name, salary)
-VALUES (?, ?, ?)
-ON CONFLICT (employee_id)
-DO UPDATE SET 
-    first_name = EXCLUDED.first_name,
-    salary = EXCLUDED.salary
-	WHERE EXCLUDED.salary > 10000
-"""
+# raw_sql = """
+# INSERT INTO Employees (employee_id, first_name, salary)
+# VALUES (?, ?, ?)
+# ON CONFLICT (employee_id)
+# DO UPDATE SET 
+#     first_name = EXCLUDED.first_name,
+#     salary = EXCLUDED.salary
+# 	WHERE EXCLUDED.salary > 10000
+# """
 
 ### Oracle
 # raw_sql = """
@@ -126,33 +126,59 @@ DO UPDATE SET
 #     INSERT (employee_id, first_name, salary) VALUES (s.employee_id, s.first_name, s.salary);
 # """
 
-rec = Record(statement=raw_sql, parameters=(100, 'Ahmed', 4000))
+# rec = Record(statement=raw_sql, parameters=(100, 'Ahmed', 4000))
 
-rs = Recordset()
+# emp = Employees()
+# emp.employee_id = 100
+# emp.read()
 
-emp1 = Employees()
-emp1.set.new = {'employee_id': 100, 'first_name': 'Ahmed', 'salary': 4000}
+### SQLite3
+# assert emp.data == {'employee_id': 100, 'first_name': 'Ahmed', 'last_name': 'King', 'email': 'steven.king@sqltutorial.org', 'phone_number': '515.123.4567', 'hire_date': '1987-06-17', 'job_id': 4, 'salary': 4000, 'commission_pct': None, 'manager_id': None, 'department_id': 9}, emp.data
+# assert emp.recordset.data == {}, emp.recordset.data
+### Oracle
+# assert emp.data == {'employee_id': 100, 'first_name': 'Ahmed', 'last_name': 'King', 'email': 'steven.king@sqltutorial.org', 'phone_number': '515.123.4567', 'hire_date': datetime(1987, 6, 17, 0, 0), 'job_id': 4, 'salary': 4000, 'commission_pct': None, 'manager_id': None, 'department_id': 9}, emp.data
+### MySQL | Postgres | Microsoft AzureSQL
+# assert emp.data == {'employee_id': 100, 'first_name': 'Ahmed', 'last_name': 'King', 'email': 'steven.king@sqltutorial.org', 'phone_number': '515.123.4567', 'hire_date': date(1987, 6, 17), 'job_id': 4, 'salary': 4000, 'commission_pct': None, 'manager_id': None, 'department_id': 9}, emp.data
 
-emp2 = Employees()
-emp2.set.new = {'employee_id': 101, 'first_name': 'Kamal', 'salary': 5000}
-
-rs.add(emp1)
-rs.add(emp2)
+#=====
+# emp1 = Employees()
+# emp1.set.new = {'employee_id': 100, 'first_name': 'Ahmed', 'salary': 4000}
 
 # emp1.upsert(onColumns='employee_id')
-emp = Employees()
-emp.employee_id = 100
-emp.read()
+
+# emp = Employees()
+# emp.employee_id = 100
+# emp.read()
+
+### SQLite3
+# assert emp.data == {'employee_id': 100, 'first_name': 'Ahmed', 'last_name': 'King', 'email': 'steven.king@sqltutorial.org', 'phone_number': '515.123.4567', 'hire_date': '1987-06-17', 'job_id': 4, 'salary': 4000, 'commission_pct': None, 'manager_id': None, 'department_id': 9}, emp.data
+# assert emp.recordset.data == {}, emp.recordset.data
+### Oracle
+# assert emp.data == {'employee_id': 100, 'first_name': 'Ahmed', 'last_name': 'King', 'email': 'steven.king@sqltutorial.org', 'phone_number': '515.123.4567', 'hire_date': datetime(1987, 6, 17, 0, 0), 'job_id': 4, 'salary': 4000, 'commission_pct': None, 'manager_id': None, 'department_id': 9}, emp.data
+### MySQL | Postgres | Microsoft AzureSQL
+# assert emp.data == {'employee_id': 100, 'first_name': 'Ahmed', 'last_name': 'King', 'email': 'steven.king@sqltutorial.org', 'phone_number': '515.123.4567', 'hire_date': date(1987, 6, 17), 'job_id': 4, 'salary': 4000, 'commission_pct': None, 'manager_id': None, 'department_id': 9}, emp.data
+#=====
+# rs = Recordset()
+
+# emp1 = Employees()
+# emp1.set.new = {'employee_id': 100, 'first_name': 'Ahmed', 'salary': 4000}
+
+# emp2 = Employees()
+# emp2.set.new = {'employee_id': 101, 'first_name': 'Kamal', 'salary': 5000}
+
+# rs.add(emp1)
+# rs.add(emp2)
 
 # rs.upsert(onColumns='employee_id')
 # print(emp1.query__.statement)
 # print(emp1.query__.parameters)
+
 # emp = Employees()
 # emp.filter(Employees.employee_id.in_([100,101]))
 # emp.read()
 
 ### SQLite3
-assert emp.data == {'employee_id': 100, 'first_name': 'Ahmed', 'last_name': 'King', 'email': 'steven.king@sqltutorial.org', 'phone_number': '515.123.4567', 'hire_date': '1987-06-17', 'job_id': 4, 'salary': 4000, 'commission_pct': None, 'manager_id': None, 'department_id': 9}, emp.data
+# assert emp.data == {'employee_id': 100, 'first_name': 'Ahmed', 'last_name': 'King', 'email': 'steven.king@sqltutorial.org', 'phone_number': '515.123.4567', 'hire_date': '1987-06-17', 'job_id': 4, 'salary': 4000, 'commission_pct': None, 'manager_id': None, 'department_id': 9}, emp.data
 # assert emp.recordset.data == {}, emp.recordset.data
 ### Oracle
 # assert emp.data == {'employee_id': 100, 'first_name': 'Ahmed', 'last_name': 'King', 'email': 'steven.king@sqltutorial.org', 'phone_number': '515.123.4567', 'hire_date': datetime(1987, 6, 17, 0, 0), 'job_id': 4, 'salary': 4000, 'commission_pct': None, 'manager_id': None, 'department_id': 9}, emp.data
@@ -516,6 +542,7 @@ assert jobs.recordset.toLists() == [[1, 'Public Accountant', 4200, 9000], [6, 'A
 assert jobs.recordset.toDicts() == [{'job_id': 1, 'job_title': 'Public Accountant', 'min_salary': 4200, 'max_salary': 9000}, {'job_id': 6, 'job_title': 'Accountant', 'min_salary': 4200, 'max_salary': 9000}]
 print("----------02A----------")
 # iterate over recordset and update records one by one: (not recommended if you can update with one predicate)
+jobs.filter(Jobs.min_salary > 1000)
 for job in jobs:
 	job.set.min_salary = 5000
 jobs.recordset.update()
