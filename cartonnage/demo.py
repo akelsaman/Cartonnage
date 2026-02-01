c7b6380f566dbe9a952f3e9d11fd6e4f5d4d3fa1
Ahmed Kamal ELSaman <ak.elsaman@gmail.com>
2026-02-02 00:07:24 +0200

    202602020007

--- cartonnage/demo.py
+++ cartonnage/demo.py
@@ -10,47 +10,45 @@ Record.database__  = sqlite3Database
 
 class Departments(Record): pass
 
-departments = Departments()
-departments.all()
+departments = Departments().all()
 print(f"{'-'*80}\nDepartments:")
-for employee in departments:
-	print(employee.data)
+for department in departments:
+	print(department.data)
 
 departments.recordset.delete()
 print(departments.query__.statement)
 print(departments.query__.parameters)
-assert departments.recordset.rowsCount == 11, f"Deleted rows count: {departments.recordset.rowsCount}"
-print(f"{'-'*80}\n{departments.recordset.rowsCount} have been deleted.")
+assert departments.recordset.rowsCount() == 11, f"Deleted rows count: {departments.recordset.rowsCount()}"
+print(f"{'-'*80}\n{departments.recordset.rowsCount()} have been deleted.")
 
 departments = Departments()
 departments.all()
 print(f"{'-'*80}")
 print(f"Departments List:{departments.recordset.toLists()}")
 
-# def initOracleConnection():
-# 	oracledb.init_oracle_client(lib_dir=AKEOracleConfig.CLIENT_LIB_DIR)
+def initOracleConnection():
+	oracledb.init_oracle_client(lib_dir=AKEOracleConfig.CLIENT_LIB_DIR)
 
-# 	load_dotenv()
-# 	connection = oracledb.connect(
-# 		user=AKEOracleConfig.USER,
-# 		password=AKEOracleConfig.PASSWORD,
-# 		dsn=AKEOracleConfig.DSN
-# 	)
-# 	return connection
+	load_dotenv()
+	connection = oracledb.connect(
+		user=AKEOracleConfig.USER,
+		password=AKEOracleConfig.PASSWORD,
+		dsn=AKEOracleConfig.DSN
+	)
+	return connection
 
-# oracleCOnnection = initOracleConnection()
-# oracleDatabase = Oracle(oracleCOnnection)
-# Record.database__ = oracleDatabase # change database connection
-# print(f"{'-'*80}\nDatabase connection has been changed from SQLite3 to Oracle.")
+oracleCOnnection = initOracleConnection()
+oracleDatabase = Oracle(oracleCOnnection)
+Record.database__ = oracleDatabase # change database connection
+print(f"{'-'*80}\nDatabase connection has been changed from SQLite3 to Oracle.")
 
-# departments = Departments()
-# departments.all()
-# print(f"{'-'*80}\n{departments.recordset.count()} have been retreived from Oracle.")
+departments = Departments().all()
+print(f"{'-'*80}\n{departments.recordset.count()} have been retreived from Oracle.")
 
-# Record.database__  = sqlite3Database
-# print(f"{'-'*80}\nDatabase connection has been changed from Oracle to SQLite3.")
-# departments.recordset.insert()
-# print(f"{'-'*80}\n{departments.recordset.rowsCount} have been saved to SQLite3.")
+Record.database__  = sqlite3Database
+print(f"{'-'*80}\nDatabase connection has been changed from Oracle to SQLite3.")
+departments.recordset.insert()
+print(f"{'-'*80}\n{departments.recordset.rowsCount()} have been saved to SQLite3.")
 
-# print(f"{'-'*80}")
-# print(f"Departments List:{departments.recordset.toLists()}")
+print(f"{'-'*80}")
+print(f"Departments List:{departments.recordset.toLists()}")
